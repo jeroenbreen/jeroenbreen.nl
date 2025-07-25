82e463ab79ef5f3015dacfd29c7e52651716cf13
Jeroen Breen <jeroen@innouveau.nl>
2025-07-25 10:01:58 +0200

    Update Rails.vue

--- components/Project/Rails.vue
+++ components/Project/Rails.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { projects } from '~/data/projects'
+import {projects} from '~/data/projects'
+import type {Project} from '~/types'
 
 const el = ref<HTMLElement | null>(null)
 const n = 3;
@@ -7,7 +8,7 @@ const n = 3;
 const scrolled = ref(0)
 
 const getSets = () => {
-    const s = Array.from({ length: n }, () => {
+    const s = Array.from({length: n}, () => {
         return {
             projects: [],
             size: 0,
@@ -17,7 +18,7 @@ const getSets = () => {
     for (let i = 0; i < projects.length; i++) {
         s[i % n].projects.push(projects[i]);
     }
-    return s
+    return s;
 }
 
 const sets = ref(getSets())
@@ -61,9 +62,9 @@ onMounted(() => {
 </script>
 
 <template>
-    <div ref="el" class="h-full grid gap-4 grid-cols-[minmax(250px,1.4fr)_minmax(150px,1.1fr)_minmax(100px,0.8fr)]">
+    <div ref="el" class="h-full grid gap-4 grid-cols-[minmax(350px,1.4fr)_minmax(100px,1.1fr)_minmax(50px,0.8fr)]">
         <div v-for="(set, index) in sets" :key="index" class="relative h-full overflow-hidden">
-            <ProjectRail  :projects="set.projects" :top="getTop(index)" />
+            <ProjectRail :projects="set.projects" :top="getTop(index)"/>
         </div>
     </div>
 </template>
